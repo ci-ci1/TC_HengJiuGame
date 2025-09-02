17dafa1bf33844cb7bd53101e74c5705d5f3820e
cbw <3521028229@qq.com>
2025-09-02 17:03:05 +0800

    9月2日增，改，查

--- Controllers/JobManageController.cs
+++ Controllers/JobManageController.cs
@@ -13,16 +13,27 @@ namespace TC_HengJiuGame.Controllers
 
         ReturnJsonListData listData = new ReturnJsonListData();
 
+        ReturnJsonData jsonData = new ReturnJsonData();
+
         // GET: JobManage
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetList(int page,int limit)
+        public ActionResult GetList(int page,int limit, string name , string code )
         {
             var list = db.Job.Where(a => a.IsDel == false).ToList();
+            if (!string.IsNullOrEmpty(name))
+            {
+                list = list.Where(a => a.JobName.Contains(name)).ToList();
+            }
+            if (!string.IsNullOrEmpty(code))
+            {
+                list = list.Where(a => a.JobCode.Contains(code)).ToList();
+            }
+
             var offset = (page - 1) * limit;
-            var newList = list.OrderBy(a => a.CreateDate).Skip(offset).Take(limit).ToList();
+            var newList = list.OrderByDescending(a => a.ModifyDate).Skip(offset).Take(limit).ToList();
 
             listData.code = 0;
             listData.msg = "";
@@ -31,6 +42,46 @@ namespace TC_HengJiuGame.Controllers
 
 
             return Json(listData,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Save(Job job)
+        {
+
+            //判断是添加还是修改
+            if (job.ID!=Guid.Empty)
+            {
+                var jobInfo = db.Job.Find(job.ID);
+
+                // 修改字段
+                jobInfo.JobName = job.JobName;
+                jobInfo.JobCode = job.JobCode;
+                jobInfo.ModifyDate = DateTime.Now;
+
+                db.Entry(jobInfo).State = System.Data.Entity.EntityState.Modified;
+
+            }
+            else
+            {
+                job.ID = Guid.NewGuid();
+                job.CreateDate = DateTime.Now;
+                job.ModifyDate = DateTime.Now;
+                job.IsDel = false;
+                db.Job.Add(job);
+            }
+            if (db.SaveChanges() > 0)
+            {
+                jsonData.code = 0;
+                jsonData.msg = "提交成功！";
+            }
+            else
+            {
+                jsonData.code = 1;
+                jsonData.msg = "提交失败！";
+            }
+
+
+
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
         }
     }
 }
